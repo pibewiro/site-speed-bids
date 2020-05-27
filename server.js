ec9f279798773d7e60d698b34e7a4592a55ab049
prince <princeibewiro@indecex.com.br>
2020-05-27 16:22:42 -0300

    deploy

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 3000;
+const history = require('connect-history-api-fallback');
 
 const app = express();
+app.use(history());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
