fc7853a89cce3a8f0b2764df90f652e0c81ab07f
prince <princeibewiro@indecex.com.br>
2020-05-27 21:07:08 -0300

    deploy

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
-const port = process.env.PORT || 3000;
+const serverStatic = require("serve-static");
 const history = require('connect-history-api-fallback');
 
 const app = express();
 app.use(history());
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use("/", serverStatic(path.join(__dirname, "dist")));
 
+let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening to Port:', port))
