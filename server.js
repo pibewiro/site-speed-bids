bbd19b7d507323f60a6763c7f3edc0173a3e6c8a
prince <princeibewiro@indecex.com.br>
2020-05-27 21:22:19 -0300

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const serverStatic = require("serve-static");
+// const serverStatic = require("serve-static");
 const history = require('connect-history-api-fallback');
 
 const app = express();
 app.use(history());
 
-app.use("/", serverStatic(path.join(__dirname, "dist")));
+app.use("/", express.static(path.join(__dirname, "dist")));
 
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('Listening to Port:', port))
