9ad43fbd9d77f5c1a73ef448737d663bedfd7e00
prince <princeibewiro@indecex.com.br>
2020-06-04 23:45:24 -0300

    addition: live bid added with timer

--- src/components/organisms/Purchases/LiveBids/LiveBid.vue
+++ src/components/organisms/Purchases/LiveBids/LiveBid.vue
@@ -1,56 +1,114 @@
 <template>
   <div>
       <h1 class="text-center mb-3">Live Bid</h1>
-
-      <div class="chatForm border">
+    <!-- <p>{{buyer}}</p> -->
+    <div class="timer">
+        <p class="border">{{timer}}</p>
+    </div>
+    <div class="chat-section">
+        <div class="product-chat border">
+            <h2 class="text-center mt-2">Product</h2>
+                <div class="pc-img-div">
+                    <img :src="`${imageUrl}/${buyer.product.image.defaultImage}`" alt="">
+                </div>
+            <div class="pc-info">
+                <p><span>Product Name:</span> {{buyer.product.productName}}</p>
+                <p><span>Owner:</span> {{buyer.owner.username}}</p>
+                <p><span>Starting Price:</span> R${{buyer.product.price}}</p>
+                <p><span>Current Bid:</span> R${{buyer.currentPrice}}</p>
+                <p><span>Last Bidder:</span> {{buyer.winner.username}}</p>
+                <p><span>Number of Bidders:</span> {{buyer.liveBidders.length}}</p>
+            </div>
+        </div>
+        <div class="chatForm border">
           <div class="messages border mb-3">
-      <p v-for="(message, i) in messages" :key="i" :class="userAuth.userId === message.userId ? 'myBid' : 'otherBid'"><span>{{message.username}}:</span> {{message.info}}</p>
+                <p v-for="(message, i) in messages" :key="i" :class="userAuth.userId === message.userId ? 'myBid' : 'otherBid'"><span>{{message.username}}:</span> {{message.info}}</p>
           </div>
           <div class="btn-div">
               <textarea v-model="bidInput" class="form-control mb-2"></textarea>
+              <p class="text-danger">{{error}}</p>
               <button @click="handleBid" class="site-btn btn btn-block">Enter</button>
           </div>
       </div>
+    </div>
       
   </div>
 </template>
 
 <script>
 import io from 'socket.io-client'
+import {mapActions, mapState} from 'vuex';
 export default {
     data:()=>({
         socket:null,
         userAuth:null,
         messages:[],
-        messages2:[],
-        bidInput:null
+        bidInput:null,
+        buyerId:null,
+        imageUrl:null,
+        error:null,
+        timer:null
     }),
 
-    methods:{
-        handleBid(){
-            this.socket.emit('bid', {price:this.bidInput, bidId:this.$route.params.liveId, userId:this.userAuth.userId, username:this.userAuth.username});
-            this.bidInput = null
+    computed:{
+        ...mapState('Buyer', ['buyer'])
+    },
+
+    watch:{
+        messages: async function(){
+            await this.getBuyer({
+            token:this.userAuth.token,
+            buyerId:this.buyerId
+        })
         }
     },
 
-    created(){
+    methods:{
+        ...mapActions('Buyer', ['getBuyer']),
+
+        handleBid(){
+            this.socket.emit('bid', {price:this.bidInput, bidId:this.buyerId, userId:this.userAuth.userId, username:this.userAuth.username});
+            this.bidInput = null;
+            this.error = null;
+        }
+    },
+
+    async created(){
         this.userAuth = JSON.parse(localStorage.getItem("_speedbids"))
+        this.imageUrl = process.env.VUE_APP_API_IMAGES;
+        this.buyerId = this.$route.params.liveId
+        await this.getBuyer({
+            token:this.userAuth.token,
+            buyerId:this.buyerId
+        })
         this.socket = io.connect(process.env.VUE_APP_API_SOCKET);
-        this.socket.emit("loggedIn", {bidId:this.$route.params.liveId, firstname:this.userAuth.firstname, lastname:this.userAuth.lastname, userId:this.userAuth.userId})
+        this.socket.emit("loggedIn", {bidId:this.buyerId, firstname:this.userAuth.firstname, lastname:this.userAuth.lastname, userId:this.userAuth.userId, username:this.userAuth.username})
         this.socket.on("logInMessage", (data)=>{
-            this.messages.push(data);
+        this.messages.push(data);
         })
 
-                    this.socket.on('bid2', (data)=>{
-                this.messages.push(data);
-            })
+        this.socket.on("timer", (data)=>{
+            this.timer = data;
+        })
+
+        this.socket.on("finished", ()=>{
+            this.$router.push("/")
+        })
+
+        this.socket.on('error', (data)=>{
+            this.error = data.error;
+        })
+
+        this.socket.on('bid2', (data)=>{
+            this.messages.push(data);
+        })
     }
 }
 </script>
 
-<style>
+<style scoped>
 .chatForm{
-    width:40%;
+    width:50%;
     margin:0 auto;
     padding: 10px;
 }
@@ -73,5 +131,53 @@ export default {
 
 span{
     font-weight: bold;
+}
+
+.chat-section{
+    display: flex;
+    justify-content: center;
+}
+
+.product-chat{
+    width: 50%;
+    height: 555px;
+    margin-right: 20px;
+}
+
+.pc-img-div{
+    margin:0 auto;
+    width: 40%;
+    height: 250px;
+    background: gray;
+}
+
+.pc-img-div img{
+    width: 100%;
+    height: 100%;
+}
+
+h2{
+    font-weight: 100;
+}
+
+span{
+    font-weight: bold;
+}
+
+.pc-info{
+    padding-left:30%;
+    padding-top:10px;
+}
+
+.timer{
+    text-align: center;
+}
+
+.timer p{
+    font-size: 30px;
+    border-radius: 10px;
+    font-weight: 100;
+    width: 100px;
+    margin:0 auto 10px auto;
 }
 </style>
--- src/store/Buyer/index.js
+++ src/store/Buyer/index.js
@@ -1,54 +1,80 @@
-import axios from 'axios';
+import axios from "axios";
 
 const state = {
   buyers: {},
+  buyer: {},
   addedPrice: null,
   liveBids: {},
 };
 
 const mutations = {
   GET_BUYERS: function (state, payload) {
-    state.buyers = payload.data
+    state.buyers = payload.data;
+  },
+
+  GET_BUYER: function (state, payload) {
+    state.buyer = payload.data;
   },
 
   ADDED_PRICE: function (state, payload) {
-    state.addedPrice = payload.data
+    state.addedPrice = payload.data;
   },
 
   VIEW_BIDS: function (state, payload) {
-    state.liveBids = payload.data
-  }
+    state.liveBids = payload.data;
+  },
 };
 
 const actions = {
   async getBuyers({ commit }, obj) {
-    const response = await axios.get(`${process.env.VUE_APP_API_ROOT}/buyer/${obj.id}`,
-      { headers: { 'x-access-token': obj.token } }
-    )
-    commit('GET_BUYERS', response.data);
+    const response = await axios.get(
+      `${process.env.VUE_APP_API_ROOT}/buyer/${obj.id}`,
+      { headers: { "x-access-token": obj.token } }
+    );
+    commit("GET_BUYERS", response.data);
+  },
+
+  async getBuyer({ commit }, obj) {
+    const response = await axios.get(
+      `${process.env.VUE_APP_API_ROOT}/buyer/get-buyer/${obj.buyerId}`,
+      {
+        headers: { "x-access-token": obj.token },
+      }
+    );
+
+    commit("GET_BUYER", response.data);
   },
 
   async addPrice({ commit }, obj) {
-    const response = await axios.put(`${process.env.VUE_APP_API_ROOT}/buyer/${obj.id}`, { ...obj.data },
-      { headers: { 'x-access-token': obj.token } }
-    )
+    const response = await axios.put(
+      `${process.env.VUE_APP_API_ROOT}/buyer/${obj.id}`,
+      { ...obj.data },
+      { headers: { "x-access-token": obj.token } }
+    );
 
-    commit('ADDED_PRICE', response.data)
+    commit("ADDED_PRICE", response.data);
   },
 
   async addLiveBidder(commit, obj) {
-    await axios.post(`${process.env.VUE_APP_API_ROOT}/buyer/liveBid`, { ...obj.data }, {
-      headers: { 'x-access-token': obj.token }
-    })
+    await axios.post(
+      `${process.env.VUE_APP_API_ROOT}/buyer/liveBid`,
+      { ...obj.data },
+      {
+        headers: { "x-access-token": obj.token },
+      }
+    );
   },
 
   async viewLiveBids({ commit }, obj) {
-    const response = await axios.get(`${process.env.VUE_APP_API_ROOT}/buyer/view-bids/${obj.userId}`, {
-      headers: { 'x-access-token': obj.token }
-    });
+    const response = await axios.get(
+      `${process.env.VUE_APP_API_ROOT}/buyer/view-bids/${obj.userId}`,
+      {
+        headers: { "x-access-token": obj.token },
+      }
+    );
 
-    commit('VIEW_BIDS', response.data);
-  }
+    commit("VIEW_BIDS", response.data);
+  },
 };
 const getters = {};
 
@@ -57,5 +83,5 @@ export default {
   state,
   mutations,
   actions,
-  getters
-}
+  getters,
+};
